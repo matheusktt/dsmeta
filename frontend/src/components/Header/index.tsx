b6e8213f82debb9973189fb94323f6d576a51850
Matheus Oliveira <matheus_0119@outlook.com>
2022-07-12 15:55:37 -0300

    Outros componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por <a href="https://github.com/matheusktt" target="_blank">Matheus Oliveira</a></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
