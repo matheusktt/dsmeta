c880a8618ef11d9da837a4bf919ff97297ceb0a7
Matheus Oliveira <matheus_0119@outlook.com>
2022-07-16 19:39:14 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg"
 import { BASE_URL } from "../../utils/request";
 import './styles.css'
@@ -9,8 +10,8 @@ type Props = {
 
 function handleClick(id : number) {
     axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-        console.log("Sucesso");
-    })
+        toast.info("SMS enviado com sucesso");
+    });
 }
 
 function NotificationButton({saleId} : Props) {
